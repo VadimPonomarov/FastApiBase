109f2a46cb63aac3e75c7a4ec804f8a7d0fe409a
Вадим <pvs.versia@gmail.com>
2025-01-20 19:13:51 +0200

    get_db method added into model User

--- src/core/models/user/user_model.py
+++ src/core/models/user/user_model.py
@@ -1,9 +1,19 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 from core.mixins.model_mixins.datetime_mixin import CrtUpdDatetimeMixin
 from core.mixins.model_mixins.id_int_mixin import IdIntMixin
 from core.models import BaseModel
+from core.types.user_types import UserIdType
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(BaseModel, IdIntMixin, CrtUpdDatetimeMixin, SQLAlchemyBaseUserTable[int]):
-    pass
+class User(
+    BaseModel, IdIntMixin, CrtUpdDatetimeMixin, SQLAlchemyBaseUserTable[UserIdType]
+):
+    @classmethod
+    async def get_db(session: "AsyncSession"):
+        yield SQLAlchemyUserDatabase(session, User)
