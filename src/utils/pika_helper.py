f4a7eb2ce5e21912ee2f2cb66823bf8f13bccf16
Вадим <pvs.versia@gmail.com>
2025-03-05 18:24:06 +0200

    pika_helper

--- src/utils/pika_helper.py
+++ src/utils/pika_helper.py
@@ -1,7 +1,7 @@
 import json
 from typing import TYPE_CHECKING
 
-import pika
+from pika.adapters.blocking_connection import BlockingConnection
 
 from core.enums.pika import ExchangeType, QueueType
 
@@ -14,27 +14,27 @@ class PikaConnection:
         self,
         parameters: "ConnectionParameters",
         queue_name: str,
-        queue_type: str = QueueType.DURABLE,
+        queue_type: QueueType = QueueType.DURABLE,
         exchange_name: str = "",
         exchange_type: ExchangeType = ExchangeType.DIRECT,
     ):
-        self.__connection = pika.BlockingConnection(parameters)
+        self.__connection = BlockingConnection(parameters)
         self.__que_name = queue_name
         self.__que_type = queue_type
         self.__exchange_name = exchange_name
         self.__exchange_type = exchange_type
 
-    def get_connection(self):
+    def get_connection(self) -> "BlockingConnection":
         return self.__connection
 
     def publish(self, message: dict):
         with self.get_connection() as connection:
             channel = connection.channel()
             channel.queue_declare(
-                queue=self.__que_name, durable=self.__que_type == "durable"
+                queue=self.__que_name, durable=self.__que_type.value == "durable"
             )
             channel.exchange_declare(
-                exchange=self.__exchange_name, exchange_type=self.__exchange_type
+                exchange=self.__exchange_name, exchange_type=self.__exchange_type.value
             )
             message_json = json.dumps(message).encode("utf-8")
             channel.basic_publish(
