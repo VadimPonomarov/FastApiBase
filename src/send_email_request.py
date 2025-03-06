5449c50b2c30df3925d372c89bf23f0b9f909c17
Вадим <pvs.versia@gmail.com>
2025-03-06 16:01:15 +0200

    pika_helper

--- src/send_email_request.py
+++ src/send_email_request.py
@@ -1,22 +1,21 @@
 from pika import ConnectionParameters
 
-from services.pika_helper import ConnectionFactory
+from services.pika_helper import ConnectionFactory, MessageSchema
 
 
-def send_email_request(to_email, subject, message, logo_url):
-    email_data = {
-        "to_email": to_email,
-        "subject": subject,
-        "message": message,
-        "logo_url": logo_url,
-    }
+def send_email_request(email_data: MessageSchema) -> None:
     ConnectionFactory(
         parameters=ConnectionParameters("localhost"),
         queue_name="email_queue",
-    ).publish(email_data)
+    ).publish(email_data.model_dump())
 
 
 if __name__ == "__main__":
     send_email_request(
-        "pvs.versia@gmail.com", "Test Subject", "This is a test message.", "cid:logo"
+        MessageSchema(
+            to_email="pvs.versia@gmail.com",
+            subject="Test Subject",
+            message="This is a test message.",
+            logo_url="cid:logo",
+        )
     )
