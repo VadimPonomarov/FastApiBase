9574f9bb954a6c72bab20c1fa1f665f4a8a3f2e9
Вадим <pvs.versia@gmail.com>
2025-03-14 16:23:19 +0200

    Template logo block without border amended

--- src/settings/logger_config.py
+++ src/settings/logger_config.py
@@ -5,7 +5,7 @@ from settings.config import settings
 ENABLE_LOGGING = settings.enable_logging
 
 if ENABLE_LOGGING:
-    logger.add(sink="stdout", level=settings.log_level or "DEBUG")
+    logger.add(sink="console", level=settings.log_level or "DEBUG")
 else:
     logger.remove()
 
